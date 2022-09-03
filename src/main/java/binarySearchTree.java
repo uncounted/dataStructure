156491085260b888c4add50997c57be2cf4bd755
hoholoudly <hoholoudly@gmail.com>
2022-09-03 14:35:30 +0900

    이진트리

--- src/main/java/binarySearchTree.java
+++ src/main/java/binarySearchTree.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class binarySearchTree {
+public class binarySearchTree {
+
+    public Node search(int searchValue, Node node) {
+        if (node == null || node.val == searchValue) {
+            return node;
+        } else if (searchValue < node.val) {
+            return search(searchValue, node.leftChild);
+        } else {
+            return search(searchValue, node.rightChild);
+        }
+    }
+
+    public void insert(int value, Node node) {
+        if (value < node.val) {
+            if (node.leftChild == null) {
+                node.leftChild = new Node(value);
+            } else {
+                insert(value, node.leftChild);
+            }
+        } else if (value > node.val) {
+            if (node.rightChild == null) {
+                node.rightChild = new Node(value);
+            } else {
+                insert(value, node.rightChild);
+            }
+        }
+    }
+
+//    public Node delete(int valueToDelete, Node node) {
+//        if (node == null) {
+//            return null;
+//        } else if (valueToDelete < node.val) {
+//            node.leftChild = delete(valueToDelete, node);
+//
+//        } else if (){
+//
+//        }
+//    }
+
+    public class Node {
+        int val;
+        Node leftChild;
+        Node rightChild;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node leftChild, Node rightChild) {
+            this.val = val;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+    }
 }
